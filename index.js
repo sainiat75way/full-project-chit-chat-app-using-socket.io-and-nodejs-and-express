4f76c164f0762ee27a8f478b494e2cee72f5c9b3
sainiat75way <156658583+sainiat75way@users.noreply.github.com>
2024-01-15 15:54:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+const io = new Server(server); // Move this line after creating the server
+
+// socket io
+io.on('connection', (socket) => {
+  socket.on('user-message', (message) =>{
+    io.emit("message", message);
+  });
+});
+
+const path = require('path');
+app.use(express.static(path.resolve("./public")));
+app.get('/', (req, res) => {
+  res.sendFile('/public/index.html');
+});
+
+server.listen(5002, () => {
+  console.log('listening on *:5002'); // Corrected port number in the log statement
+});
